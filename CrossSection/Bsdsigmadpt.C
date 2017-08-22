29ee59711b3746e96ea0c325f7d1548e6423c6cd
ljeure <ljeure@gmail.com>
2017-08-22 16:01:57 +0200

    fixed fonll code

--- CrossSection/Bsdsigmadpt.C
+++ CrossSection/Bsdsigmadpt.C
@@ -9,40 +9,38 @@
 #define BIN_NUM 220 //pPb_pt:220,pPb_y:40,pp_pt:222,pp_y:45
 #define REBIN 1     //pPb_pt:6,pPb_y:4,pp_pt:8,pp_y:4
 #define REBINp 2    //pPb_pt:7,pPb_y:5,pp_pt:9,pp_y:5
-#define HMIN 5      //pPb_pt:5,pPb_y:-2,pp_pt:9,pp_y:0
+#define HMIN 10      //pPb_pt:5,pPb_y:-2,pp_pt:9,pp_y:0
 #define HMAX 60     //pPb_pt:55,pPb_y:2,pp_pt:120,pp_y:2.25
 
-int Bsdsigmadpt()
-{
+int Bsdsigmadpt() {
 
   gROOT->SetStyle("Plain");
   gStyle->SetOptTitle(0);
   gStyle->SetOptStat(0);
 
-  ifstream getdata("../FONLLInputs/fo_pPb_pt_rap24.dat");
+  ifstream getdata("fonllInputs/fonllInput.dat");
 
-  if(!getdata.is_open())
-    {
-      cout<<"Opening the file fails"<<endl;
-    }
+  if(!getdata.is_open()) {
+    cout<<"Opening the file fails"<<endl;
+  }
 
   float central[BIN_NUM];
-  float min_all[BIN_NUM],max_all[BIN_NUM],min_sc[BIN_NUM],max_sc[BIN_NUM],min_mass[BIN_NUM],max_mass[BIN_NUM],min_pdf[BIN_NUM],max_pdf[BIN_NUM];
+  float min_all[BIN_NUM],max_all[BIN_NUM],min_sc[BIN_NUM],max_sc[BIN_NUM],
+        min_mass[BIN_NUM],max_mass[BIN_NUM],min_pdf[BIN_NUM],max_pdf[BIN_NUM];
   int i;
   float tem;
-  for(i=0;i<BIN_NUM;i++)
-    {
-      getdata>>tem;
-      getdata>>central[i];
-      getdata>>min_all[i];
-      getdata>>max_all[i];
-      getdata>>min_sc[i];
-      getdata>>max_sc[i];
-      getdata>>min_mass[i];
-      getdata>>max_mass[i];
-      getdata>>min_pdf[i];
-      getdata>>max_pdf[i];
-    }
+  for(i=0;i<BIN_NUM;i++) {
+    getdata>>tem;
+    getdata>>central[i];
+    getdata>>min_all[i];
+    getdata>>max_all[i];
+    getdata>>min_sc[i];
+    getdata>>max_sc[i];
+    getdata>>min_mass[i];
+    getdata>>max_mass[i];
+    getdata>>min_pdf[i];
+    getdata>>max_pdf[i];
+  }
   
   TH1F* hpt = new TH1F("hpt","",BIN_NUM,HMIN,HMAX);
   TH1F* hminall = new TH1F("hminall","",BIN_NUM,HMIN,HMAX);
@@ -54,18 +52,18 @@ int Bsdsigmadpt()
   TH1F* hminpdf = new TH1F("hminpdf","",BIN_NUM,HMIN,HMAX);
   TH1F* hmaxpdf = new TH1F("hmaxpdf","",BIN_NUM,HMIN,HMAX);
 
-  for(i=0;i<BIN_NUM;i++)
-    {
-      hpt->SetBinContent(i+1,central[i]);
-      hminall->SetBinContent(i+1,min_all[i]);
-      hmaxall->SetBinContent(i+1,max_all[i]);
-      hminsc->SetBinContent(i+1,min_sc[i]);
-      hmaxsc->SetBinContent(i+1,max_sc[i]);
-      hminmass->SetBinContent(i+1,min_mass[i]);
-      hmaxmass->SetBinContent(i+1,max_mass[i]);
-      hminpdf->SetBinContent(i+1,min_pdf[i]);
-      hmaxpdf->SetBinContent(i+1,max_pdf[i]);
-    }
+  for(i=0;i<BIN_NUM;i++) {
+    hpt->SetBinContent(i+1,central[i]);
+    hminall->SetBinContent(i+1,min_all[i]);
+    hmaxall->SetBinContent(i+1,max_all[i]);
+    hminsc->SetBinContent(i+1,min_sc[i]);
+    hmaxsc->SetBinContent(i+1,max_sc[i]);
+    hminmass->SetBinContent(i+1,min_mass[i]);
+    hmaxmass->SetBinContent(i+1,max_mass[i]);
+    hminpdf->SetBinContent(i+1,min_pdf[i]);
+    hmaxpdf->SetBinContent(i+1,max_pdf[i]);
+  }
+
   //Rebin Edge
   double rebin[REBINp] = {10,60};
 
@@ -81,9 +79,12 @@ int Bsdsigmadpt()
 
   //bin middle
   double apt[REBIN] = {35.};//pPb_pt
+  //
   //bin half width
   double aptl[REBIN] = {25.};//pPb_pt
-  double asigma[REBIN],aminall[REBIN],amaxall[REBIN],aminsc[REBIN],amaxsc[REBIN],aminmass[REBIN],amaxmass[REBIN],aminpdf[REBIN],amaxpdf[REBIN],aerrorl[REBIN],aerrorh[REBIN];
+  double asigma[REBIN],aminall[REBIN],amaxall[REBIN],aminsc[REBIN],
+         amaxsc[REBIN],aminmass[REBIN],amaxmass[REBIN],aminpdf[REBIN],
+         amaxpdf[REBIN],aerrorl[REBIN],aerrorh[REBIN];
 
   //number of every rebined bin
   double bin_num[REBIN] = {200.};//pPb_pt
@@ -91,51 +92,51 @@ int Bsdsigmadpt()
   int j;
   double norm=1.;
   
-  for(j=0;j<REBIN;j++)
-    {
+  for(j=0;j<REBIN;j++) {
 
-      tem = hpt_rebin->GetBinContent(j+1);
-      asigma[j] = tem*norm/bin_num[j];
+    tem = hpt_rebin->GetBinContent(j+1);
+    asigma[j] = tem*norm/bin_num[j];
 
-      tem = hminall_rebin->GetBinContent(j+1);
-      aminall[j] = tem*norm/bin_num[j];
+    tem = hminall_rebin->GetBinContent(j+1);
+    aminall[j] = tem*norm/bin_num[j];
 
-      tem = hmaxsc_rebin->GetBinContent(j+1);
-      amaxall[j] = tem*norm/bin_num[j];
+    tem = hmaxsc_rebin->GetBinContent(j+1);
+    amaxall[j] = tem*norm/bin_num[j];
 
-      tem = hminsc_rebin->GetBinContent(j+1);
-      aminsc[j] = tem*norm/bin_num[j];
+    tem = hminsc_rebin->GetBinContent(j+1);
+    aminsc[j] = tem*norm/bin_num[j];
 
-      tem = hmaxsc_rebin->GetBinContent(j+1);
-      amaxsc[j] = tem*norm/bin_num[j];
+    tem = hmaxsc_rebin->GetBinContent(j+1);
+    amaxsc[j] = tem*norm/bin_num[j];
 
-      tem = hminmass_rebin->GetBinContent(j+1);
-      aminmass[j] = tem*norm/bin_num[j];
+    tem = hminmass_rebin->GetBinContent(j+1);
+    aminmass[j] = tem*norm/bin_num[j];
 
-      tem = hmaxmass_rebin->GetBinContent(j+1);
-      amaxmass[j] = tem*norm/bin_num[j];
+    tem = hmaxmass_rebin->GetBinContent(j+1);
+    amaxmass[j] = tem*norm/bin_num[j];
 
-      tem = hminpdf_rebin->GetBinContent(j+1);
-      aminpdf[j] = tem*norm/bin_num[j];
+    tem = hminpdf_rebin->GetBinContent(j+1);
+    aminpdf[j] = tem*norm/bin_num[j];
 
-      tem = hmaxpdf_rebin->GetBinContent(j+1);
-      amaxpdf[j] = tem*norm/bin_num[j];
+    tem = hmaxpdf_rebin->GetBinContent(j+1);
+    amaxpdf[j] = tem*norm/bin_num[j];
 
-      aerrorl[j] = asigma[j]-aminall[j];//all,sc,mass,pdf
-      aerrorh[j] = amaxall[j]-asigma[j];//all,sc,mass,pdf
-    }
+    aerrorl[j] = asigma[j]-aminall[j];//all,sc,mass,pdf
+    aerrorh[j] = amaxall[j]-asigma[j];//all,sc,mass,pdf
+  }
 
   cout<<"------- pPb_5.02------"<<endl;
   cout<<endl;
  
-  TGraphAsymmErrors* gae = new TGraphAsymmErrors(REBIN, apt, asigma, aptl, aptl, aerrorl, aerrorh);
+  TGraphAsymmErrors* gae = 
+    new TGraphAsymmErrors(REBIN, apt, asigma, aptl, aptl, aerrorl, aerrorh);
   gae->SetTitle(";p_{T}(GeV/c);d#sigma (B admix) /dp_{T}(pb c/GeV)");
   gae->SetFillColor(2);
   gae->SetFillStyle(3001);
 
   TCanvas* cr = new TCanvas("cr","cr",600,500);
   cr->SetLogy();
-  TH2F* hempty=new TH2F("hempty","",10,5,60.,10.,10,100000000);  
+  TH2F* hempty = new TH2F("hempty","",10,5,60.,10.,10,100000000);  
   hempty->GetXaxis()->SetTitle("p_{t} (GeV/c)");
   hempty->GetYaxis()->SetTitle("d#sigma(B admix)/dp_{T}(pb/GeV)");
   hempty->GetXaxis()->SetTitleOffset(1.);
@@ -184,13 +185,16 @@ int Bsdsigmadpt()
   
   for (int i=0;i<gaeSigmaDecay->GetN();i++){
     gaeSigmaDecay->GetY()[i] *= BRFraction*norm;
-    gaeSigmaDecay->SetPointEYhigh(i,gaeSigmaDecay->GetErrorYhigh(i)*BRFraction*norm);
-    gaeSigmaDecay->SetPointEYlow(i,gaeSigmaDecay->GetErrorYlow(i)*BRFraction*norm); 
+    gaeSigmaDecay->SetPointEYhigh(
+        i,gaeSigmaDecay->GetErrorYhigh(i)*BRFraction*norm);
+    gaeSigmaDecay->SetPointEYlow(
+        i,gaeSigmaDecay->GetErrorYlow(i)*BRFraction*norm); 
   }
   
   gaeSigmaDecay->SetFillColor(2);
   gaeSigmaDecay->SetFillStyle(3001); 
-  gaeSigmaDecay->SetTitle(";p_{T}(GeV/c);d#sigma(B^{+} full chain)/dp_{T} #times A (GeV^{-1}c)");
+  gaeSigmaDecay->SetTitle(
+      ";p_{T}(GeV/c);d#sigma(B^{+} full chain)/dp_{T} #times A (GeV^{-1}c)");
    
   TH2F* hempty=new TH2F("hempty","",10,0,70.,10.,10000.,5000000000);  
   hempty->GetXaxis()->SetTitle("p_{T} (GeV/c)");
@@ -223,7 +227,7 @@ int Bsdsigmadpt()
 
   canvas->SaveAs("Plots/canvasBs.eps");
   canvas->SaveAs("Plots/canvasBs.pdf");
-  TFile*foutput=new TFile("../outputBs.root","recreate");
+  TFile*foutput=new TFile("outputBs.root","recreate");
   foutput->cd();
   gae->Write();
   gaeSigmaDecay->Write();
