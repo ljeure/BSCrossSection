9dbeb860ce8ef56fe01562a06f17fa3bf09c78d3
ljeure <ljeure@gmail.com>
2017-09-02 20:51:32 +0200

    made readme and added some comments

--- CrossSection/doAnalysis.sh
+++ CrossSection/doAnalysis.sh
@@ -6,15 +6,16 @@ CENTPbPbMIN=0
 CENTPbPbMAX=100
 #Central point of the analysis
 
+# these settings control which macros are run
 DOANALYSISPP_FONLL=0
 DOANALYSISPP_FIT=1
 DOANALYSISPP_MCSTUDY=1
 DOANALYSISPP_CROSS=1
 
-DOANALYSISPbPb_FIT=0
-DOANALYSISPbPb_MCSTUDY=0
-DOANALYSISPbPb_CROSS=0
-DORAA=0
+DOANALYSISPbPb_FIT=1
+DOANALYSISPbPb_MCSTUDY=1
+DOANALYSISPbPb_CROSS=1
+DORAA=1
 
 ## PP MONTE CARLO
 INPUTMCPP="../../bsTMVACDozen/merged-files/MLP_and_BDT_MC.root"
@@ -39,10 +40,6 @@ ISMCPP=0
 ISDOWEIGHTPP=0
 SELGENPP="TMath::Abs(Gy)<2.4&&abs(GpdgId)==521&&GisSignal==1"
 
-#pp GA
-
-#CUTPP="TMath::Abs(By)<2.4&&TMath::Abs(Bmumumass-3.096916)<0.15&&Bmass>5&&Bmass<6&&Btrk1Pt>.687&&Btrk2Pt>.654&&Bchi2cl>.00431&&BsvpvDistance/BsvpvDisErr>3.05&&Bd0/Bd0Err>212&&cos(Bdtheta)>-.232&&Bmu1pt>.88&&Bmu2pt>.589&&Bpt>5.75&&abs(Btktkmass-1.019455)<.0384&&Blxy>-.0106"
-
 # Candan's unoptimized cuts (used in Julia's slides)
 CUTPP="TMath::Abs(By)<2.4&&TMath::Abs(Bmumumass-3.096916)<0.15&&Bmass>5&&Bmass<6&&Btrk1Pt>1.5&&Btrk2Pt>1.5&&Bchi2cl>.0132&&BsvpvDistance/BsvpvDisErr>3.05&&Bd0/Bd0Err>2&&cos(Bdtheta)>.26&&Bmu1pt>1.5&&Bmu2pt>1.5&&Bpt>5.75&&abs(Btktkmass-1.019455)<.0155&&Blxy>.025"
 RECOONLYPP=$CUTPP
@@ -55,6 +52,7 @@ OUTPUTFILEPlotPP="ROOTfiles/CrossSectionPP.root"
 OUTPUTFILEPbPb="ROOTfiles/hPtSpectrumBsPbPb.root"
 
 #SETTING for NP fit
+# this setting for the fit was causing problems. The fit is assumed to be a linear plus a gaussian in the code  as of 9/1/2017
 #NPFIT_PP="3.12764e1*TMath::Gaus(x,5.33166,3.64663e-2)*(x<5.33166)+(x>=5.33166)*3.12764e1*TMath::Gaus(x,5.33166,1.5204e-1)+2.11124e2*TMath::Erf(-(x-5.14397)/6.43194e-2) + 2.11124e2"
 NPFIT_PP="0"
 
@@ -111,7 +109,8 @@ OUTPUTFILEMCSTUDYPbPb="ROOTfiles/MCstudiesPbPb.root"
 OUTPUTFILEPlotPbPb="ROOTfiles/CrossSectionPbPb.root"
 
 #SETTING for NP fit
-NPFIT_PbPb="3.39711e1*TMath::Gaus(x,5.35002e0,3.87952e-2)*(x<5.35002e0)+(x>=5.35002e0)*3.39711e1*TMath::Gaus(x,5.35002e0,1.14232e-1)+2.16376e2*TMath::Erf(-(x-5.14189)/8.66243e-2) + 2.16376e2"
+#NPFIT_PbPb="3.39711e1*TMath::Gaus(x,5.35002e0,3.87952e-2)*(x<5.35002e0)+(x>=5.35002e0)*3.39711e1*TMath::Gaus(x,5.35002e0,1.14232e-1)+2.16376e2*TMath::Erf(-(x-5.14189)/8.66243e-2) + 2.16376e2"
+NPFIT_PbPb="0"
 
 if [ $DOANALYSISPbPb_FIT -eq 1 ]; then      
 g++ fitBs.C $(root-config --cflags --libs) -g -o fitBs.exe 
--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# Instructions for pp references
-In this repository you can find the code to build the pp reference of the B meson measurements in pp collisions at 5.02 TeV starting from the FONLL calculations. The input files .dat were produced using http://www.lpthe.jussieu.fr/~cacciari/fonll/fonllform.html using the following parameters for the calculations:
+This repository contains code to perform the RAA calculation for bs analysis. The macro is run by going to the CrossSection directory and doing "source doAnalysis.sh".
+
+doAnalysis.sh sets a bunch of parameters and runs 5 different macros. Each of the macros is outlined below.
+
+# Bsdsigmadpt
+Bsdsigmadpt.cc contains code to build the pp reference of the B meson measurements in pp collisions at 5.02 TeV starting from the FONLL calculations. The fonll input file (CrossSection/fonllInputs/fonllInput.dat) were produced using http://www.lpthe.jussieu.fr/~cacciari/fonll/fonllform.html using the following parameters for the calculations:
 
 Collider: LHC(pp, 5.5), Heavy quark: botton ,PDFs: CTEQ6.6
 
@@ -9,11 +13,31 @@ Cross section type: dsigma/dpt (or dsigma/dy) uncertainty range from scales and 
 
 Include PDFs uncertainties: (missing for the moment)
 
-The file fo_pp_Bmeson5_5TeV.dat was produced with the kinematic ranges:
+The file fonllInput.dat was produced with the kinematic ranges:
 
-pt min = 2 GeV
-pt max = 200 GeV
+pt min = 10 GeV
+pt max = 60 GeV
 y min= -2.4
 y max= 2.4
 Use y:check
 npoints=793
+
+Bsdsigmapt.cc outputs a file that can be used for both pp and PbPb fiting calculations
+
+# fitBs
+applies cuts to the data and fits a gaussian and quadratic function to the data.
+
+fitBs can be run for both pp and PbPb
+
+# MCefficiency
+calculates MCefficiency which is needed in to run CrossSectionRatio if the doDataCor parameter is set to 1. If in CrossSectionRatio the doDataCor parameter is set to 0, MCefficiency is not needed.
+
+MCefficiency can be run for both pp and PbPb
+
+# CrossSectionRatio
+CrossSectionRatio plots the theoretical cross section (the output from Bsdsigmadpt) and the calculated cross section (the output from MCefficiency)
+
+CrossSectionRatio can be run for both pp and PbPb
+
+# NuclearModificationFactor
+calculates the RAA
